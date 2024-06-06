c4156f4fafcbac73927b51271a3d49e6801381ce
sutharprit2k26 <pritsuthar.dev@gmail.com>
2024-06-06 19:02:58 +0530

    vercel SpeedInsights

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import ReactDOM from 'react-dom/client';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
   </React.StrictMode>
 );
 
